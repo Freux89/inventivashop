e534fe081cee16fc9d0bc756a1c6c9f2340d908a
Freux89 <cr.pavone@benantiadv.com>
2024-07-25 16:20:49 +0200

    Migliorata navigazione descrizione prodotto

--- resources/views/frontend/default/pages/partials/products/description.blade.php
+++ resources/views/frontend/default/pages/partials/products/description.blade.php
@@ -1,9 +1,10 @@
 <div class="product-info-tab bg-white rounded-2 overflow-hidden pt-6 my-9">
-    <ul class="nav nav-tabs border-bottom justify-content-center gap-5 pt-info-tab-nav">
-        <li><a href="#description" class="active" data-bs-toggle="tab">{{ localize('Description') }}</a></li>
-        <li><a href="#info" data-bs-toggle="tab">{{ localize('Additional Information') }}</a></li>
-
-    </ul>
+<div class="custom-nav-tabs-wrapper">
+        <ul class="nav nav-tabs custom-nav-tabs border-bottom justify-content-center gap-5 pt-info-tab-nav">
+            <li><a href="#description" class="active" data-bs-toggle="tab">{{ localize('Description') }}</a></li>
+            <li><a href="#info" data-bs-toggle="tab">{{ localize('Additional Information') }}</a></li>
+        </ul>
+    </div>
     <div class="tab-content">
         <div class="tab-pane fade show active px-4 py-5" id="description">
             @if ($product->description)
