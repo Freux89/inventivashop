11d4ca9ed5c55d1ca4781db2edc82cd42fd67324
Freux89 <cr.pavone@benantiadv.com>
2024-08-29 16:41:29 +0200

    ripeto modifica

--- app/Http/Resources/ProductVariationInfoResource.php
+++ app/Http/Resources/ProductVariationInfoResource.php
@@ -130,7 +130,7 @@ class ProductVariationInfoResource extends JsonResource
         $summaryBoxVariantsHtml = view('frontend.default.pages.partials.products.summary-box-variants-content', [
             'productVariations' => $filteredProductVariations,
         ])->render();
-dd($conditionEffects['disableVariationValuesMap']);
+
         return [
             'filteredIds' => $filteredIds,
             'recap_body_html' => $recapBodyHtml,
