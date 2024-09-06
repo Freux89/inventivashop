2ee140e1d51d5aa80e7e803f9607224c2d6c6e41
Freux89 <cr.pavone@benantiadv.com>
2024-09-06 16:19:12 +0200

    Nelle sezioni contenuto il contenuto immagine caricava il tag a anche se non veniva inserito un url

--- resources/views/frontend/default/pages/partials/sections/image.blade.php
+++ resources/views/frontend/default/pages/partials/sections/image.blade.php
@@ -1,9 +1,14 @@
 <div class="{{ $item->type == 'only-content' ? 'pe-md-6' : '' }} {{ $columnLayout[$index] == 3 ? 'col-6' : 'col-12' }} col-md-{{ $columnLayout[$index] }} p-0 py-{{ $item->settings['columnPaddingY'] }} px-md-{{ $item->settings['columnPaddingX'] }}  mb-6 mb-md-0 {{ isset($item->settings['image']) && !isset($item->settings['title']) ? 'd-flex align-items-center justify-content-center' : '' }} {{ $index < count($section->items) - 1 && isset($section->settings['divider']) ? 'divider-border-right' : '' }}">
 
 @if (isset($item->settings['image']))
-    <a href="{{ $item->settings['imageUrl'] }}" title="{{ $item->settings['titleUrl'] }}">
-        <img src="{{ uploadedAsset($item->settings['image']) }}" class="img-fluid img-adaptive-height" alt="{{ $item->settings['altImage'] }}">
-    </a>
+    @if (!empty($item->settings['imageUrl']))
+        <a href="{{ $item->settings['imageUrl'] }}" title="{{ $item->settings['titleUrl'] }}">
     @endif
+        <img src="{{ uploadedAsset($item->settings['image']) }}" class="img-fluid img-adaptive-height" alt="{{ $item->settings['altImage'] }}">
+    @if (!empty($item->settings['imageUrl']))
+        </a>
+    @endif
+@endif
+
 
 </div>
